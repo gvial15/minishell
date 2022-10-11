5d95e786bc6906457b0bdb3f60a0d3caff4eadeb
mraymond <quadrayxl@gmail.com>
2022-10-11 10:11:13 -0400

    my change never append on my last commit, try again

--- src/0_main.c
+++ src/0_main.c
@@ -6,7 +6,7 @@
 /*   By: mraymond <mraymond@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 18:27:46 by gvial             #+#    #+#             */
-/*   Updated: 2022/10/06 10:21:42 by mraymond         ###   ########.fr       */
+/*   Updated: 2022/10/11 10:06:10 by mraymond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -79,6 +79,7 @@ int	main(int ac, char **av, char **envp)
 			parse(envp, ms);
 			print_cmd_lst(ms->cmds);
 			free_cmds(ms->cmds);
+			exec(ms);
 		}
 		ms->cmds = NULL;
 	}
